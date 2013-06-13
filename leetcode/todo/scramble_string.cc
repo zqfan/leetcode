6389c10130935015e0ff678e08ca2d83fbae2975
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-06-13 16:37:27 +0800

    mark some problem as todo

---
+++ leetcode/todo/scramble_string.cc
@@ -0,0 +1,20 @@
+/* Copyright 2013 <ZhiQiang Fan>
+
+   Given a string s1, we may represent it as a binary tree by
+   partitioning it to two non-empty substrings recursively.
+
+   Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+   To scramble the string, we may choose any non-leaf node and swap
+   its two children.
+
+   Given two strings s1 and s2 of the same length, determine if s2 is
+   a scrambled string of s1.
+*/
