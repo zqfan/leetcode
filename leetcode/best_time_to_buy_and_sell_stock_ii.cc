fe389856bb881f6180baa9ba87a0f3ab18f80f04
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-07-06 14:31:38 +0800

    refactor best time to buy and sell stock ii

--- leetcode/best_time_to_buy_and_sell_stock_ii.cc
+++ leetcode/best_time_to_buy_and_sell_stock_ii.cc
@@ -1,13 +1,26 @@
-/* Copyright 2013 <ZhiQiang Fan>
+/* Copyright (C) 2013 ZhiQiang Fan <aji.zqfan@gmail.com>
 
-   Say you have an array for which the ith element is the price of a
-   given stock on day i.
-   Design an algorithm to find the maximum profit. You may complete as
-   many transactions as you like (ie, buy one and sell one share of
-   the stock multiple times). However, you may not engage in multiple
-   transactions at the same time (ie, you must sell the stock before
-   you buy again).
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+/* Say you have an array for which the ith element is the price of a
+given stock on day i.
+Design an algorithm to find the maximum profit. You may complete as
+many transactions as you like (ie, buy one and sell one share of the
+stock multiple times). However, you may not engage in multiple
+transactions at the same time (ie, you must sell the stock before you
+buy again). */
 #include <cstdio>
 #include <vector>
 using std::vector;
@@ -16,16 +29,11 @@ class Solution {
   public:
   int maxProfit(vector<int> &prices) {
     int max_profit = 0;
-
-    if (prices.size() < 1)
-      return 0;
-
-    for (int i = 0; i < prices.size() - 1; i++) {
-      if (prices[i] - prices[i+1] < 0) {
+    for (size_t i = 0; i + 1 < prices.size(); i++) {
+      if (prices[i+1] - prices[i] > 0) {
         max_profit += prices[i+1] - prices[i];
       }
     }
-
     return max_profit;
   }
 };
