0e9425a028df7f88f7be4de6c4086216b7e77e39
ZhiQiang Fan <zhiqiang.fan@huawei.com>
2015-05-28 17:20:10 +0800

    solve 217 contains duplicate

---
+++ leetcode/python/217_contains_duplicate.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2015 ZhiQiang Fan <aji.zqfan@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice
+# in the array, and it should return false if every element is distinct.
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean}
+    def containsDuplicate(self, nums):
+        uniq = {}
+        for num in nums:
+            if num in uniq:
+                return True
+            uniq[num] = 1
+        return False
