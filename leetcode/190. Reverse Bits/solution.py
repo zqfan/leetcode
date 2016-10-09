34110889127c97716e855cd8fa7dc7c0d98431f0
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-10-10 03:18:34 +0800

    resolve 190. Reverse Bits way 2

--- leetcode/190. Reverse Bits/solution.py
+++ leetcode/190. Reverse Bits/solution.py
@@ -5,3 +5,12 @@ class Solution:
         n_str = list(bin(n)[:1:-1])
         n_str += ['0'] * (32 - len(n_str))
         return int('0b' + ''.join(n_str), 2)
+
+    def reverseBits2(self, n):
+        ret = 0
+        for i in range(32):
+            b = n & 1
+            rb = b << (31 - i)
+            ret |= rb
+            n >>= 1
+        return ret
