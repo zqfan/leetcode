e4021978aeacd836785aaf6eaf93b9641e0ae9b7
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-10-10 22:49:24 +0800

    resolve 189. Rotate Array way 2

--- leetcode/189. Rotate Array/solution.py
+++ leetcode/189. Rotate Array/solution.py
@@ -5,6 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        if (not nums) or (k % len(nums)) == 0:
+            return
+
+        def swap(start):
+            for i in range(k):
+                nums[start+i], nums[l-k+i] = nums[l-k+i], nums[start+i]
+
+        l = len(nums)
+        start = 0
+        k = k % l
+        while start < l and k != 0:
+            k = k % (l - start)
+            swap(start)
+            start += k
+
+    def rotate1(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
         l = len(nums)
         k = k % l
         nums[:] = nums[l-k:] + nums[:l-k]
