71ea5a3f8fdfb75005a6e12c7e6ff728977e2f69
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-10-13 00:29:37 +0800

    resolve 268. Missing Number

---
+++ leetcode/268. Missing Number/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(nums) * (len(nums) + 1) / 2 - sum(nums)
