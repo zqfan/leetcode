a458ffeff9ebbb0f69d1a650e8779c9f21553568
ZhiQiang Fan <zhi-qiang.fan@hpe.com>
2016-11-06 02:47:39 -0700

    update 438. Find All Anagrams in a String

--- leetcode/438. Find All Anagrams in a String/solution.py
+++ leetcode/438. Find All Anagrams in a String/solution.py
@@ -8,26 +8,21 @@ class Solution(object):
         if not s or len(s) < len(p):
             return []
 
-        indices = []
-        primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,
-                  53,59,61,67,71,73,79,83,89,97,101]
-
-        product_p = 1
+        finger_p = {}
         for c in p:
-            product_p *= primes[ord(c) - ord('a')]
+            finger_p[c] = finger_p.get(c, 0) + 1
 
-        product_s = 1
+        finger_s = {}
         for i in range(len(p)-1):
-            product_s *= primes[ord(s[i]) - ord('a')]
+            finger_s[s[i]] = finger_s.get(s[i], 0) + 1
 
-        for j in range(len(p)-1, len(s)):
-            product_s *= primes[ord(s[j]) - ord('a')]
-            if product_s == product_p:
-                indices.append(j-len(p)+1)
-            product_s /= primes[ord(s[j-len(p)+1]) - ord('a')]
+        indices = []
+        for i in range(len(p)-1, len(s)):
+            finger_s[s[i]] = finger_s.get(s[i], 0) + 1
+            if finger_s == finger_p:
+                indices.append(i-len(p)+1)
+            finger_s[s[i-len(p)+1]] -= 1
+            if finger_s[s[i-len(p)+1]] == 0:
+                finger_s.pop(s[i-len(p)+1])
 
         return indices
-
-# test
-s = Solution().findAnagrams
-assert s("cbaebabacd", "abc") == [0, 6]
