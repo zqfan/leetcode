1f459579ebd4ddb470bb6791a10bd3d4f6496168
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-11-08 01:13:35 -0700

    update 215. Kth Largest Element in an Array

--- leetcode/215. Kth Largest Element in an Array/solution.py
+++ leetcode/215. Kth Largest Element in an Array/solution.py
@@ -5,36 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        heap = [0] * k
-        heap[1:] = nums[:k]
-        self.init_min_heap(heap)
-        for i in range(k, len(nums)):
-            if nums[i] > heap[1]:
-                heap[1] = nums[i]
-                self.adjust_min_heap(heap, 1)
-        return heap[1]
+        return Solution.find_kth_num(nums, k, 0, len(nums))
 
     @staticmethod
-    def init_min_heap(heap):
-        for i in range(len(heap)/2, 0, -1):
-            Solution.adjust_min_heap(heap, i)
-
-    @staticmethod
-    def adjust_min_heap(heap, i):
-        heap[0] = heap[i]
-        j = i * 2
-        while j < len(heap):
-            if j+1 < len(heap) and heap[j] > heap[j+1]:
-                j += 1
-            if heap[0] <= heap[j]:
-                break
+    def find_kth_num(nums, k, start, stop):
+        if stop - start < 2:
+            return nums[start]
+        r = nums[random.randrange(start, stop)]
+        e = u = start
+        g = stop
+        while u < g:
+            if nums[u] > r:
+                nums[u], nums[e] = nums[e], nums[u]
+                e += 1
+                u += 1
+            elif nums[u] < r:
+                g -= 1
+                nums[u], nums[g] = nums[g], nums[u]
             else:
-                heap[j/2] = heap[j]
-                j *= 2
-        heap[j/2] = heap[0]
-
-# test
-s = Solution().findKthLargest
-import random
-l = [random.randint(-32767, 32767) for i in range(100)]
-assert s(l, 1) == sorted(l)[-1]
+                u += 1
+        if k <= (e-start):
+            return Solution.find_kth_num(nums, k, start, e)
+        elif k > (g-start):
+            return Solution.find_kth_num(nums, k-(g-start), g, stop)
+        else:
+            return nums[e]
