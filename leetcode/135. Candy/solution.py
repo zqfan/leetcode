70074d988c2e88bdf1b21e098bab2a93d7036539
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-11-09 10:04:34 -0700

    resolve 135. Candy

---
+++ leetcode/135. Candy/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        l = len(ratings)
+        candies = [1] * l
+        for i in range(1, l):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = max(candies[i], candies[i-1]+1)
+        for i in range(l-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1]+1)
+        return sum(candies)
