bda9b4b155394d40313b22eb999410b16fb5abaf
ZhiQiang Fan <aji.zqfan@gmail.com>
2016-11-08 23:06:19 -0700

    resolve 406. Queue Reconstruction by Height

---
+++ leetcode/406. Queue Reconstruction by Height/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        heap = [[k, h, i] for i, (h, k) in enumerate(people)]
+        ordered_queue = []
+        while heap:
+            heapq.heapify(heap)
+            ordered_queue.append(people[heapq.heappop(heap)[2]])
+            for element in heap:
+                if ordered_queue[-1][0] >= element[1]:
+                    element[0] -= 1
+        return ordered_queue
