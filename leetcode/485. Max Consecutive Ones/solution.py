e32a9a41b5eb4ef2562319119ba243b75e3f781d
ZhiQiang Fan <aji.zqfan@gmail.com>
2017-01-18 23:26:43 -0700

    resolve 485. Max Consecutive Ones

---
+++ leetcode/485. Max Consecutive Ones/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return max(map(len, ''.join(map(str, nums)).split('0')))
