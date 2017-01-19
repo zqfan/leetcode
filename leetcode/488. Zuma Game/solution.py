5ede743430c6fb401bc4c010b9ef89b60d7ba918
ZhiQiang Fan <aji.zqfan@gmail.com>
2017-01-19 06:47:50 -0700

    update 488. Zuma Game

--- leetcode/488. Zuma Game/solution.py
+++ leetcode/488. Zuma Game/solution.py
@@ -7,9 +7,10 @@ class Solution(object):
         """
         def dfs(board, hand):
             if not board:
-                history[board] = 0
+                return 0
             if board in history:
                 return history[board]
+            # we can do minimal check to skip some larger step process
             minimal = -1
             for i in xrange(len(board)):
                 for j in xrange(len(hand)):
@@ -17,32 +18,19 @@ class Solution(object):
                     if board[i] != hand[j]:
                         continue
                     new_board = board[:i] + hand[j] + board[i:]
-                    new_board = self.remove_balls(new_board)
-                    new_hand = hand[:j] + hand[j+1:]
-                    step = dfs(new_board, new_hand)
+                    reduced_board = self.remove_balls(new_board)
+                    step = dfs(reduced_board, hand[:j] + hand[j+1:])
                     if step >= 0 and (minimal == -1 or minimal > step + 1):
                         minimal = step + 1
-            history[board] = minimal
-            return minimal
-        # we can do input check to skip some impossible case
-        # we can do minimal check to skip some larger step process
+            return history.setdefault(board, minimal)
+        # we can do input check to skip some impossible case, for one or two balls
         history = {}
         return dfs(board, hand)
 
     def remove_balls(self, board):
-        count = start = pre = 0
-        for i, ball in enumerate(board):
-            if ball == pre:
-                count += 1
-            elif count < 2:
-                pre = ball
-                count = 0
-                start = i
-            else:
-                break
-        if count >= 2:
-            end = start + count + 1
-            new_board = board[:start] + board[end:]
-            return self.remove_balls(new_board)
-        else:
-            return board
+        # we can do one pass check to accelerate it
+        for dup in ['R'*3, 'G'*3, 'B'*3, 'Y'*3, 'W'*3]:
+            start, end = board.find(dup), board.rfind(dup) + 3
+            if start != -1:
+                return self.remove_balls(board[:start] + board[end:])
+        return board
