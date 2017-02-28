d5c14c1dccf2fe5a952109c3b8c251ee0795760d
ZhiQiang Fan <aji.zqfan@gmail.com>
2017-03-01 00:52:41 +0800

    update 516. Longest Palindromic Subsequence

--- leetcode/516. Longest Palindromic Subsequence/solution.py
+++ leetcode/516. Longest Palindromic Subsequence/solution.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        def helper(start, end):
-            if start > end:
-                return 0
-            if mem[start][end]:
-                return mem[start][end]
-            if s[start] != s[end]:
-                mem[start][end] = max(helper(start, end-1), helper(start+1, end))
-            elif start != end:
-                mem[start][end] = helper(start+1, end-1) + 2
-            else:
-                mem[start][end] = 1
-            return mem[start][end]
-
-        mem = [[0] * len(s) for _ in xrange(len(s))]
-        return helper(0, len(s)-1)
+        # based on @jedihy
+        dp = [1] * len(s)
+        for end in xrange(len(s)):
+            for start in reversed(xrange(end)):
+                if s[start] == s[end]:
+                    m = 2 + pre if start < end - 1 else 2
+                else:
+                    m = max(dp[start+1], dp[start])
+                pre, dp[start] = dp[start], m
+        return dp[0] if dp else 0
