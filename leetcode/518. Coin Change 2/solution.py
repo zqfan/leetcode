57f5bb443bf06ff7ff070bfb6be4e9ffdf459511
ZhiQiang Fan <aji.zqfan@gmail.com>
2017-03-08 18:00:02 +0800

    resolve 518. Coin Change 2

---
+++ leetcode/518. Coin Change 2/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for c in coins:
+            for i in xrange(c, amount + 1):
+                dp[i] += dp[i - c]
+        return dp[amount]
